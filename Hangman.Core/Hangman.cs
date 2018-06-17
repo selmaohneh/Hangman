26dcda33fbb0b6c41baa7f954d6da33b9e8546e0
Marcel Neumann <marcelneumann@fastmail.com>
2018-06-17 10:34:27 +0200

    All tests working, even full sample game.

--- Hangman.Core/Hangman.cs
+++ Hangman.Core/Hangman.cs
@@ -1,19 +1,79 @@
-﻿using System.Linq;
+﻿using System.Collections.Generic;
 
 namespace Hangman.Core
 {
     public class Hangman
     {
         private readonly string _searchedWord;
+        private readonly IList<char> _foundLetters = new List<char>();
 
         public Hangman(string searchedWord)
         {
             _searchedWord = searchedWord;
         }
 
-        public string GuessLetter(char letter)
+        public string GuessLetter(char guessedLetter)
         {
-            return "_ _ _ _ _ _ _ _ _";
+            if (GuessIsCorrect(guessedLetter))
+            {
+                AddToFoundLetters(guessedLetter);
+            }
+
+            var output = GenerateOutput();
+
+            return output;
+        }
+        
+        private bool GuessIsCorrect(char guessedLetter)
+        {
+            return _searchedWord.ContainsLetterAsLowerOrUpper(guessedLetter);
+        }
+
+        private void AddToFoundLetters(char guessedLetter)
+        {
+            if (!_foundLetters.ContainsLetterAsLowerOrUpper(guessedLetter))
+            {
+                _foundLetters.Add(guessedLetter);
+            }
+        }
+
+        private string GenerateOutput()
+        {
+            var output = "";
+            for (var index = 0; index < _searchedWord.Length; index++)
+            {
+                var searchedLetter = _searchedWord[index];
+
+                if (index != 0)
+                {
+                    output += " ";
+                }
+
+                if (_foundLetters.ContainsLetterAsLowerOrUpper(searchedLetter))
+                {
+                    output += searchedLetter;
+                }
+                else
+                {
+                    output += "_";
+                }
+            }
+
+            return output;
+        }
+    }
+
+    internal static class Extensions
+    {
+        public static bool ContainsLetterAsLowerOrUpper(this IList<char> letters, char letter)
+        {
+            return letters.Contains(char.ToLowerInvariant(letter)) 
+                   || letters.Contains(char.ToUpperInvariant(letter));
+        }
+
+        public static bool ContainsLetterAsLowerOrUpper(this string word, char letter)
+        {
+            return word.ToCharArray().ContainsLetterAsLowerOrUpper(letter);
         }
     }
 }
--- Hangman.Tests/HangmanTests.cs
+++ Hangman.Tests/HangmanTests.cs
@@ -20,5 +20,84 @@ namespace Hangman.Tests
 
             Assert.AreEqual("_ _ _ _ _ _ _ _ _", output);
         }
+
+        [TestMethod]
+        public void CorrectGuess()
+        {
+            var output = _hangman.GuessLetter('r');
+
+            Assert.AreEqual("_ _ _ _ _ _ _ _ r", output);
+        }
+
+        [TestMethod]
+        public void CorrectGuess_ThenWrongGuess()
+        {
+            var output = _hangman.GuessLetter('r');
+            Assert.AreEqual("_ _ _ _ _ _ _ _ r", output);
+
+            output = _hangman.GuessLetter('a');
+            Assert.AreEqual("_ _ _ _ _ _ _ _ r", output);
+        }
+
+        [TestMethod]
+        public void CorrectGuess_ThenWrongGuess_ThenCorrectGuess()
+        {
+            var output = _hangman.GuessLetter('r');
+            Assert.AreEqual("_ _ _ _ _ _ _ _ r", output);
+
+            output = _hangman.GuessLetter('a');
+            Assert.AreEqual("_ _ _ _ _ _ _ _ r", output);
+
+            output = _hangman.GuessLetter('e');
+            Assert.AreEqual("_ e _ e _ _ _ e r", output);
+        }
+
+        [TestMethod]
+        public void UpperCaseGuess()
+        {
+            var output = _hangman.GuessLetter('D');
+            Assert.AreEqual("D _ _ _ _ _ _ _ _", output);
+        }
+
+        [TestMethod]
+        public void LowerCaseGuessIsValid_EvenIfSearchedLetterIsUpperCase()
+        {
+            var output = _hangman.GuessLetter('d');
+            Assert.AreEqual("D _ _ _ _ _ _ _ _", output);
+        }
+
+        [TestMethod]
+        public void FullSampleGame()
+        {
+            var output = _hangman.GuessLetter('u');
+            Assert.AreEqual("_ _ _ _ _ _ _ _ _", output);
+
+            output = _hangman.GuessLetter('e');
+            Assert.AreEqual("_ e _ e _ _ _ e _", output);
+
+            output = _hangman.GuessLetter('n');
+            Assert.AreEqual("_ e _ e _ _ _ e _", output);
+
+            output = _hangman.GuessLetter('o');
+            Assert.AreEqual("_ e _ e _ o _ e _", output);
+
+            output = _hangman.GuessLetter('r');
+            Assert.AreEqual("_ e _ e _ o _ e r", output);
+
+            output = _hangman.GuessLetter('a');
+            Assert.AreEqual("_ e _ e _ o _ e r", output);
+
+            output = _hangman.GuessLetter('d');
+            Assert.AreEqual("D e _ e _ o _ e r", output);
+
+            output = _hangman.GuessLetter('l');
+            Assert.AreEqual("D e _ e l o _ e r", output);
+
+            output = _hangman.GuessLetter('p');
+            Assert.AreEqual("D e _ e l o p e r", output);
+
+            output = _hangman.GuessLetter('v');
+            Assert.AreEqual("D e v e l o p e r", output);
+        }
     }
 }
