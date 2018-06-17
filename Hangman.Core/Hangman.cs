473355a6da2f53b1bf37be175277aa09212a89f0
Marcel Neumann <marcelneumann@fastmail.com>
2018-06-17 09:24:38 +0200

    Add constructor.

---
+++ Hangman.Core/Hangman.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Hangman.Core
+{
+    public class Hangman
+    {
+        public Hangman(string searchedWord)
+        {
+
+        }
+    }
+}
---
+++ Hangman.Tests/HangmanTests.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Hangman.Tests
+{
+    [TestClass]
+    internal class HangmanTests
+    {
+        private Core.Hangman _hangman;
+
+        [TestInitialize]
+        private void Init()
+        {
+            _hangman = new Core.Hangman("Developer");
+        }
+    }
+}
